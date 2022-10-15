566cd433e74dd7d38bb6371ff238764643b26b3c
naochaLuwang <somoarambam123@gmail.com>
2022-10-15 14:25:58 +0530

    graph api updated

--- pages/form/stats/[gid].js
+++ pages/form/stats/[gid].js
@@ -13,104 +13,103 @@ import { Bar } from "react-chartjs-2";
 import dbConnect from "../../../utils/db";
 
 const GraphStats = ({ formFeedback }) => {
-  // ChartJS.register(
-  //   CategoryScale,
-  //   LinearScale,
-  //   BarElement,
-  //   Title,
-  //   Tooltip,
-  //   Legend
-  // );
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
-  // const options = {
-  //   responsive: true,
-  //   plugins: {
-  //     legend: {
-  //       position: "bottom",
-  //     },
-  //     title: {
-  //       display: true,
-  //       text: `${formFeedback[0]?.formName} Bar Chart`,
-  //       font: { size: 24, color: "red" },
-  //     },
-  //   },
-  // };
-  // const labels = [];
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: `${formFeedback[0]?.formName} Bar Chart`,
+        font: { size: 24, color: "red" },
+      },
+    },
+  };
+  const labels = [];
 
-  // const getLables = () => {
-  //   for (
-  //     let i = formFeedback[0].minimumWeightage;
-  //     i <= formFeedback[0].maximumWeightage;
-  //     i++
-  //   ) {
-  //     labels.push(i);
-  //   }
-  // };
+  const getLables = () => {
+    for (
+      let i = formFeedback[0].minimumWeightage;
+      i <= formFeedback[0].maximumWeightage;
+      i++
+    ) {
+      labels.push(i);
+    }
+  };
 
-  // getLables();
+  getLables();
 
-  // formFeedback[0].form[0].options.map((text) => {
-  //   labels.push(text.optionText, -1);
-  // });
+  formFeedback[0].form[0].options.map((text) => {
+    labels.push(text.optionText, -1);
+  });
 
-  // const datas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const datas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-  // formFeedback.map((form, i) => {
-  //   switch (form.overallScore) {
-  //     case 1:
-  //       datas[i] += 1;
+  formFeedback.map((form, i) => {
+    switch (form.overallScore) {
+      case 1:
+        datas[i] += 1;
 
-  //       break;
-  //     case 2:
-  //       datas[1] += 1;
-  //       break;
-  //     case 3:
-  //       datas[2] += 1;
-  //       break;
-  //     case 4:
-  //       datas[3] += 1;
-  //       break;
-  //     case 5:
-  //       datas[4] += 1;
-  //       break;
-  //     case 6:
-  //       datas[5] += 1;
-  //       break;
-  //     case 7:
-  //       datas[6] += 1;
-  //       break;
-  //     case 8:
-  //       datas[7] += 1;
-  //       break;
-  //     case 9:
-  //       datas[8] += 1;
-  //       break;
-  //     case 10:
-  //       datas[9] += 1;
+        break;
+      case 2:
+        datas[1] += 1;
+        break;
+      case 3:
+        datas[2] += 1;
+        break;
+      case 4:
+        datas[3] += 1;
+        break;
+      case 5:
+        datas[4] += 1;
+        break;
+      case 6:
+        datas[5] += 1;
+        break;
+      case 7:
+        datas[6] += 1;
+        break;
+      case 8:
+        datas[7] += 1;
+        break;
+      case 9:
+        datas[8] += 1;
+        break;
+      case 10:
+        datas[9] += 1;
 
-  //     default:
-  //       break;
-  //   }
-  // });
+      default:
+        break;
+    }
+  });
 
-  // console.log(datas);
+  console.log(datas);
 
-  // const data = {
-  //   labels,
-  //   datasets: [
-  //     {
-  //       label: "Submitted",
-  //       data: datas,
-  //       backgroundColor: "rgba(255, 99, 132, 0.5)",
-  //     },
-  //   ],
-  // };
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Submitted",
+        data: datas,
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
 
   console.log(formFeedback);
   return (
     <div className="max-w-6xl mx-auto mt-10 h-screen">
-      {/* <Bar options={options} data={data} /> */}
-      <h1>Form Feedback</h1>
+      <Bar options={options} data={data} />
     </div>
   );
 };
