e5197e266a413b266748378dddcfe99a884481c4
naochaLuwang <somoarambam123@gmail.com>
2022-11-07 17:01:31 +0530

    fixed export error

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -485,6 +485,7 @@ const Dashboard = ({ form }) => {
                     </button> */}
 
                     <button
+                      onClick={onExportClick}
                       className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 "
                       data-rounded="rounded-md"
                       data-primary="blue-600"
